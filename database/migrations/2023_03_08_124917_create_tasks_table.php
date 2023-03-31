06186499a2d2f4d3ba55dc06bddebc685784c0de
takayuki1010 <takayuki.tokusatu8@gmail.com>
2023-03-31 12:41:53 +0900

    todo

---
+++ database/migrations/2023_03_08_124917_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            // 外部キー制約
+            $table->integer('folder_id')->unsigned();
+            $table->string('title', 100);
+            $table->integer('status')->default(1);
+            $table->date('due_date');
+            $table->timestamps();
+
+            $table->foreign('folder_id')->references('id')->on('folders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
